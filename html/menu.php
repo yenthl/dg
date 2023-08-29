748b21c03b32cca04116c4b5d8466335d755bb81
yenthl <yenthlokobet@gmail.com>
2023-08-30 00:37:13 +0200

    30/08/2023 00:37

--- html/menu.php
+++ html/menu.php
@@ -1,4 +1,7 @@
 <!DOCTYPE html>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300&family=Wavefont:wght@300&display=swap');
+</style>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -12,12 +15,15 @@
         <span class="t"></span>
         <span class="t"></span>
 
-        <span class="obj"></span>
-        <span class="obj"></span>
-        <span class="obj"></span>
-        <span class="obj"></span>
-        <span class="obj"></span>
+        <span class="obj" id="n1" onclick="tab()"><p>search</p></span>
+        <span class="obj" id="n2" onclick="tab_1()"><p>cart</p></span>
+        <span class="obj" id="n3" onclick="tab_2()"><p>receipts</p></span>
+
     </nav>
+    <div class="tab">
+        <span class="close"></span>
+        <input type="text" name="search" id="" class="srch">
+    </div>
     <script src="../js/menu.js"></script>
 </body>
 </html>
--- js/menu.js
+++ js/menu.js
@@ -1,16 +1,31 @@
 const menu = document.getElementById("menu");
-const t = document.getElementsByClassName("t");
+let t = document.getElementsByClassName("t");
+let obj = document.getElementsByClassName("obj");
 let is_locked = 1;
-console.log(menu)
+console.log(t)
 
 function unlock() {
     if (is_locked) {
-        menu.setAttribute("style","height: 450px; width: 180px;")
-        
+        menu.setAttribute("style","height: 300px; width: 180px;")
+        for (let i = 0; i < t.length; i++) {
+            t[i].style.display = "none";
+        }
+        for (let i = 0; i < obj.length; i++) {
+            obj[i].style.display = "block";
+        }
         is_locked = 0;
     }
     else{
         menu.setAttribute("style","height: 40px; width: 40px;")
+        for (let i = 0; i < t.length; i++) {
+            t[i].style.display = "block";
+        }
+        for (let i = 0; i < obj.length; i++) {
+            obj[i].style.display = "none";
+        }
         is_locked = 1;
     }
+}
+function tab(num) {
+    
 }
