67a027e9e8159604de47a5c24c0dd93422b0008f
yenthl <yenthlokobet@gmail.com>
2023-09-01 23:07:22 +0200

    01/09/23 23:07

--- html/home.php
+++ html/home.php
@@ -1,4 +1,3 @@
-<!DOCTYPE html>
 <?php
 session_start();
 include '../html/menu.php';
@@ -7,7 +6,6 @@ include '../html/menu.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>login</title>
     <link rel="stylesheet" href="../css/home.css">
 </head>
 <body>
--- html/menu.php
+++ html/menu.php
@@ -6,27 +6,33 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>login</title>
+    <title>P-FIND</title>
+    <link rel="icon" href="../resource/pf.png">
     <link rel="stylesheet" href="../css/menu.css">
 </head>
 <body>
-    <nav id="menu" class="menu" onclick="unlock()" >
-        <span class="t"></span>
-        <span class="t"></span>
-        <span class="t"></span>
+        <nav id="menu" class="menu" onclick="unlock()" >
+            <span class="t"></span>
+            <span class="t"></span>
+            <span class="t"></span>
 
-        <span class="obj" id="n1" onclick="open_tab(0)"><p>search</p></span>
-        <span class="obj" id="n2" onclick="open_tab(1)"><p>cart</p></span>
-        <span class="obj" id="n3" onclick="open_tab(2)"><p>receipts</p></span>
-        <a class="obj" id="n4" href="http://nigga.com/"><p>leave</p></a>
-
-    </nav>
+            <span class="obj" id="n1" onclick="open_tab(0)"><p>search</p></span>
+            <span class="obj" id="n2" onclick="open_tab(1)"><p>cart</p></span>
+            <span class="obj" id="n3" onclick="open_tab(2)"><p>me</p></span>
+            <span class="obj" id="n4" onclick="alert('leave')"><p>leave</p></span>
+        </nav>
+        <div class="profil"></div>
     <div class="tab" id="tab" >
         <div class="close" onclick="close_tab()">
             <span class="x" ></span>
         </div>
         <div class="search_tab results_container menu_el" id="results_container">
-            <span class="result"></span>
+            <span class="result" onclick="alert('pay nigga')">
+                <p class="name">nigga</p>
+                <p class="level">3</p>
+                <p class="state">HS</p>
+                <p class="age">15</p>
+            </span>
         </div>
         <input type="text" name="search" id="srch" class="search_tab srch menu_el">
     </div>
